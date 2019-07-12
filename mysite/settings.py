7b25cfce0be88bcbce49b1afaf5535398c97e19b
daki9 <regalade.davy@gmail.com>
2019-07-12 11:40:14 -0400

    recherche du pourquoi sa ne fonctionne pas 6

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,11 +149,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
 
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 
 # Toggle sandbox mode (when running in DEBUG mode)
