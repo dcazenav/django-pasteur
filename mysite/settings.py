dec6771fcfa9e47adbc670faf4c11e8e83accc87
daki9 <regalade.davy@gmail.com>
2019-07-12 11:20:03 -0400

    recherche du pourquoi sa ne fonctionne pas 5

--- mysite/settings.py
+++ mysite/settings.py
@@ -155,3 +155,8 @@ EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+
+# Toggle sandbox mode (when running in DEBUG mode)
+SENDGRID_SANDBOX_MODE_IN_DEBUG=True
+# echo to stdout or any other file-like object that is passed to the backend via the stream kwarg.
+SENDGRID_ECHO_TO_STDOUT=True
