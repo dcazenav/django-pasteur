d4c29b3d86bd08a73bd9180278aa1c10574e7631
daki9 <regalade.davy@gmail.com>
2019-07-15 08:51:40 -0400

    correctif feuille calcul admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,9 +148,3 @@ if os.environ.get('ENV') == 'PRODUCTION':
     DATABASES['default'].update(db_from_env)
 
 
-SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
